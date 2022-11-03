f2f90554a164ac20a97c0cd5b96709704f8c1452
FarooqKhattak <ahmadfarooq192@gmail.com>
2022-11-03 20:14:12 +0500

    fixes

--- app/src/main/java/com/mobivone/favrecipes/view/fragments/DishDetailsFragment.kt
+++ app/src/main/java/com/mobivone/favrecipes/view/fragments/DishDetailsFragment.kt
@@ -1,10 +1,8 @@
 package com.mobivone.favrecipes.view.fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
@@ -27,6 +25,15 @@ class DishDetailsFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onCreateView(
